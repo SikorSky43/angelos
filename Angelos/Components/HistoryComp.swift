93fadf250aa39cf8c0e0cb10814f579649e669b3
SikorSky43 <geekihiri@gmail.com>
2025-11-22 15:49:26 +0100

    fixed balance not showing up and added history transaction with load mde and picture of assets

---
+++ Angelos/Components/HistoryComp.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryComp.swift
+//  Angelos
+//
+//  Created by BlackBird on 22/11/25.
+//
+
+import Foundation
---
+++ Angelos/Models/HistoryItem.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryItem.swift
+//  Angelos
+//
+//  Created by BlackBird on 22/11/25.
+//
+
+import Foundation
